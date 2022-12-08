22e106ee736f7751a0d76800047a4498ced08bf0
sieun-Woo <116184724+sieun-Woo@users.noreply.github.com>
2022-12-09 01:06:32 +0900

    PostResponseDto클래스에 작성 시간 필드 추가

--- src/main/java/com/blog/myblog/dto/PostResponseDto.java
+++ src/main/java/com/blog/myblog/dto/PostResponseDto.java
@@ -1,17 +1,27 @@
 package com.blog.myblog.dto;
 
 import com.blog.myblog.entity.Post;
+import com.blog.myblog.entity.Timestamped;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Getter
 public class PostResponseDto {
+
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
     private String username;
     private String title;
     private String contents;
 
     public PostResponseDto(Post post) {
+        this.createdAt = post.getCreatedAt();
+        this.modifiedAt = post.getCreatedAt();
         this.username = post.getUsername();
         this.title  = post.getTitle();
         this.contents = post.getContents();
--- src/main/java/com/blog/myblog/entity/Timestamped.java
+++ src/main/java/com/blog/myblog/entity/Timestamped.java
@@ -15,7 +15,6 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Timestamped {
-
     @CreatedDate
     private LocalDateTime createdAt;
 
